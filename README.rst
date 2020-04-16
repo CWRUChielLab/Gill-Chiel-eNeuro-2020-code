ee9a7743004cde544d9f655a1e1e8f6fc5013cfc
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2020-04-16 17:13:55 -0400

    Add Jupyter notebook and environment file

--- README.rst
+++ README.rst
@@ -6,3 +6,19 @@ This repository holds code that performs analyses and generates figures publishe
 Gill JP, Chiel HJ (in press) Rapid adaptation to changing mechanical load by ordered recruitment of identified motor neurons. eNeuro.
 
 Data for this project is stored in a sister repository on GIN: https://gin.g-node.org/jpgill86/Gill-Chiel-eNeuro-2020-data
+
+To install dependencies and launch the Jupyter notebook, install conda_ and then run the following::
+
+    cd Gill-Chiel-eNeuro-2020-code
+    conda env create -f environment.yml -n Gill-Chiel-eNeuro-2020-code
+    conda activate Gill-Chiel-eNeuro-2020-code
+    jupyter notebook Gill-Chiel-eNeuro-2020-code.ipynb
+
+You may then execute the entire notebook from top to bottom, which will
+
+1. Download all of the data files from GIN_ (~97 MB)
+2. Generate figure files
+3. Generate tables of statistics
+
+.. _conda:  https://docs.conda.io/projects/conda/en/latest/user-guide/install/
+.. _GIN:    https://gin.g-node.org/jpgill86/Gill-Chiel-eNeuro-2020-data
---
+++ utils.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Tools for working with Neo objects
+"""
+
+import numpy as np
+import neo
+from pylttb import lttb
+from neurotic.gui.config import _neo_epoch_to_dataframe
+
+
+def DownsampleNeoSignal(sig, decimation_factor):
+    """
+
+    """
+
+    assert sig.shape[1] == 1, 'Can only downsample single-channel signals'
+
+    num_points = int(sig.shape[0] / decimation_factor)
+
+    x_downsampled, y_downsampled = lttb(
+        x=sig.times.magnitude,
+        y=sig.magnitude[:, 0],
+        threshold=num_points,
+    )
+
+    sig_downsampled = neo.IrregularlySampledSignal(
+        times=x_downsampled,
+        signal=y_downsampled,
+        units=sig.units,
+        time_units=sig.times.units,
+    )
+    return sig_downsampled
+
+def BehaviorsDataFrame(neo_epochs, behavior_query, subepoch_queries):
+    """
+
+    """
+
+    # filter epochs to obtain the behaviors
+    df = _neo_epoch_to_dataframe(neo_epochs)
+    df = df.rename(columns={'Start (s)': 'Start', 'End (s)': 'End', 'Duration (s)': 'Duration'})
+    df = df.query(behavior_query)
+    df = df.rename(columns={'Start': 'Start (s)', 'End': 'End (s)', 'Duration': 'Duration (s)'})
+
+    # add defaults for subepoch columns
+    for col_prefix in subepoch_queries:
+        df[col_prefix+' start (s)'] = np.nan
+        df[col_prefix+' end (s)'] = np.nan
+        df[col_prefix+' duration (s)'] = np.nan
+        df[col_prefix+' type'] = ''
+        df[col_prefix+' label'] = ''
+
+    # for every behavior, identify other epochs that belong to it
+    df2 = _neo_epoch_to_dataframe(neo_epochs)
+    df2 = df2.rename(columns={'Start (s)': 'Start', 'End (s)': 'End', 'Duration (s)': 'Duration'})
+    for i in df.index:
+
+        # these variables are accessible within queries when the
+        # @ symbol appears before their names, e.g. @behavior_start
+        behavior_start    = df.loc[i, 'Start (s)']
+        behavior_end      = df.loc[i, 'End (s)']
+        behavior_duration = df.loc[i, 'Duration (s)']
+        behavior_type     = df.loc[i, 'Type']
+        behavior_label    = df.loc[i, 'Label']
+
+        for col_prefix, query in subepoch_queries.items():
+
+            # the query string may optionally be paired (in a tuple) with a
+            # string, either 'first' or 'last', which indicates how to handle
+            # multiple matching epochs -- if no such string is provided,
+            # multiple matches will raise an exception
+            if isinstance(query, tuple):
+                query, position = query
+            else:
+                position = None
+
+            df3 = df2.query(query)
+
+            if len(df3) == 0:
+                # skip if there are no matches
+                continue
+            elif len(df3) == 1 or position == 'first':
+                matching_epoch = df3.iloc[0]
+            elif position == 'last':
+                matching_epoch = df3.iloc[-1]
+            else:
+                # error if there are multiple matches and a method for handling
+                # them was not explicitly specified
+                raise Exception(f'More than one epoch was found for the ' \
+                                f'behavior spanning [{behavior_start}, ' \
+                                f'{behavior_end}] that matches this query: ' \
+                                f'{query}')
+
+            df.loc[i, col_prefix+' start (s)'] = matching_epoch['Start']
+            df.loc[i, col_prefix+' end (s)'] = matching_epoch['End']
+            df.loc[i, col_prefix+' duration (s)'] = matching_epoch['Duration']
+            df.loc[i, col_prefix+' type'] = matching_epoch['Type']
+            df.loc[i, col_prefix+' label'] = matching_epoch['Label']
+
+    return df
